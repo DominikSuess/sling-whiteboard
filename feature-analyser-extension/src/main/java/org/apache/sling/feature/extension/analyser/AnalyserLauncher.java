97419e45f365f62740cf1a6689e05ccfad194c83
Dominik Suess <suess@adobe.com>
2019-05-28 14:50:49 +0200

    adjusting to  configure AnalyserTasks correctly & workaround missing configuration options for ExtensionHanlder (save location for regions information)

--- feature-analyser-extension/src/main/java/org/apache/sling/feature/extension/analyser/AnalyserLauncher.java
+++ feature-analyser-extension/src/main/java/org/apache/sling/feature/extension/analyser/AnalyserLauncher.java
@@ -19,10 +19,24 @@ package org.apache.sling.feature.extension.analyser;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.sling.feature.ArtifactId;
 import org.apache.sling.feature.Feature;
 import org.apache.sling.feature.analyser.Analyser;
 import org.apache.sling.feature.analyser.AnalyserResult;
+import org.apache.sling.feature.analyser.task.impl.CheckApiRegions;
+import org.apache.sling.feature.analyser.task.impl.CheckApiRegionsDependencies;
+import org.apache.sling.feature.analyser.task.impl.CheckApiRegionsDuplicates;
+import org.apache.sling.feature.analyser.task.impl.CheckApiRegionsOrder;
+import org.apache.sling.feature.analyser.task.impl.CheckBundleExportsImports;
+import org.apache.sling.feature.analyser.task.impl.CheckBundlesForResources;
+import org.apache.sling.feature.analyser.task.impl.CheckRequirementsCapabilities;
 import org.apache.sling.feature.builder.ArtifactProvider;
 import org.apache.sling.feature.io.file.ArtifactManager;
 import org.apache.sling.feature.io.file.ArtifactManagerConfig;
@@ -54,16 +68,42 @@ public class AnalyserLauncher implements Launcher {
                 }
             }
         });
-        
+       
         this.analyserRunner = new Runnable() {
             
+            @SuppressWarnings("serial")
             @Override
             public void run() {
                  try {
-                    Analyser analyser = new Analyser(scanner);
+                     String location = System.getProperty("sling.feature.apiregions.resource.bundles.properties");
+                     File bundleprosrc = new File(location);
+
+                     Path original = bundleprosrc.toPath();
+                     Path target = Paths.get("fmtemp/group_assembled_1.0.0/regionOrigins.properties");
+                     Files.createDirectories(target.getParent());
+                     Files.copy(original, target, StandardCopyOption.REPLACE_EXISTING); 
+                                     
+                     CheckApiRegions car = new CheckApiRegions();
+                     CheckApiRegionsOrder caro = new CheckApiRegionsOrder();
+                     CheckApiRegionsDependencies cardep = new CheckApiRegionsDependencies();
+                     CheckApiRegionsDuplicates cardup = new CheckApiRegionsDuplicates();
+                     CheckBundleExportsImports cbei = new CheckBundleExportsImports();
+                     CheckRequirementsCapabilities crc = new CheckRequirementsCapabilities();
+                     CheckBundlesForResources cbfr = new CheckBundlesForResources();
+                     Map<String, Map<String,String>> analyserTaskConfigs = new HashMap<>();
+
+                     analyserTaskConfigs.put(caro.getId(), new HashMap<String, String>(){{
+                         put("order", System.getProperty("regionsorder", ""));
+                     }});
+                     analyserTaskConfigs.put("all", new HashMap<String, String>(){{
+                         put("fileStorage", target.getParent().getParent().toAbsolutePath().toString());
+                     }});
+                    Analyser analyser = new Analyser(scanner, analyserTaskConfigs,
+                                                    car, caro, cardep, cardup, cbei, crc, cbfr);
+                     
                     final AnalyserResult result = analyser.analyse(app);
                     for (final String msg : result.getWarnings()) {
-                        context.getLogger().warn(msg);
+                        context.getLogger().warn(msg);  
                     }
                     for (final String msg : result.getErrors()) {
                         context.getLogger().error(msg);
@@ -72,6 +112,8 @@ public class AnalyserLauncher implements Launcher {
                     if (!result.getErrors().isEmpty()) {
                         context.getLogger().error("Analyser detected errors. See log output for error messages.");
                     }
+                    
+                
                 } catch (Exception e) {
                     context.getLogger().error(e.getMessage());
                     System.exit(1);
